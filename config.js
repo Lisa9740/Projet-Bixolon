676fc2418e86476b692a2a27ac3b191537be273c
Lisa9740 <barretalison@gmail.com>
2018-07-18 11:28:48 +0400

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.admin={};
+
+config.app={};
+
+config.admin.name="Alison";
+config.admin.email="";
+config.app.name="Bixolon Print";
+config.app.url="";
+
+module.exports=config
